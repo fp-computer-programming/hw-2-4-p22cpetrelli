9cefd1ef9d4099448e8fbb320ff48c8c5cba2b5a
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-09-23 11:01:34 -0400

    add file

---
+++ hw3-1-#.py
@@ -0,0 +1,26 @@
+# Author CJP 9/23/21
+# question1
+question1 = (3 * 5) / (2 + 3)
+print(question1)
+
+# question2
+question2 = ((7 + 9) ** .5) * 2
+print(question2)
+
+# question3
+question3 = (4 - 7) ** 3
+print(question3)
+
+# question4
+question4 = (-19 + 100) ** .5
+print(question4)
+
+# question5
+a = 1
+b = 3
+c = -4
+question5a = (-b + (((b ** 2) - (4 * a * c)) ** .5)) / (2 * a)
+print(question5a)
+question5b = (-b - (((b ** 2) - (4 * a * c)) ** .5)) / (2 * a)
+print(question5b)
+
